0a5b9f73b6d4f4a973979357f9dbfcf528b501f8
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2023-06-11 14:51:14 -0300

    ajuste do arquivo php

--- enviar_email.php
+++ enviar_email.php
@@ -4,9 +4,9 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-// require 'PHPMailer\src\PHPMailer.php';
-// require 'PHPMailer\src\SMTP.php';
-// require 'PHPMailer\src\Exception.php';
+require 'PHPMailer\src\PHPMailer.php';
+require 'PHPMailer\src\SMTP.php';
+require 'PHPMailer\src\Exception.php';
 
 require 'vendor/autoload.php';
 
