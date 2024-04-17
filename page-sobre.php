f9123271fbe39c7e3b56ed6c7758413bae5f33c9
Ramon Ribeiro Noleto <95661913249@itajai.local>
2024-04-17 14:20:41 -0300

    Conteudo dinamico página sobre

--- page-sobre.php
+++ page-sobre.php
@@ -11,8 +11,7 @@
                 <div class="about-text">
                     <h2><?php the_title(); ?></h2>
                     <h5>Desenvolvedor Web.</h5>
-                    <p>Gosto de trabalhar em equipe, compartilhar conhecimento e enfrentar novos desafios. Sou uma pessoa comunicativa e criativa, gosto de criar coisas novas e adquirir novos conhecimentos. Sou apaixonado por tecnologia, livros, filmes, series e games, principalmente os que me fazem pensar.</p>
-                    <p>Sou tecnólogo em Redes de Computadores, pós graduado em Engenharia de Software, tenho experiência de trabalho com infraestrutura de redes, gerenciamento de times e projetos, atendimento ao cliente, e desenvolvimento web.</p>
+                    <p><?php echo get_post_meta(get_the_ID(), 'sobre', true) ?></p>
                     <a href="https://wa.me/5596981403089?text=Ol%C3%A1%2C+achei+seu+contato+pelo+seu+site%21" target="_blank">
                         <button type="submit">Fale comigo</button>
                     </a>
